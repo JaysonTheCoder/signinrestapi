ca78dd09a60642fc0c8761eb96bc564f55f2bcd6
JaysonTheCoder <jaysonbonina6@gmail.com>
2024-04-05 09:19:13 -0700

    inital commit

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express")
+const app = express() 
+const mysql = require("mysql")
+const cors = require('cors')
+const port = 4040
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+
+const connection = mysql.createConnection({
+    host    : 'sql6.freesqldatabase.com',
+    user    : 'sql6695400',
+    password: 'U9Bwq5aM2T',
+    database: 'sql6695400'
+})
+
+connection.connect((err)=> {
+    if(err) throw err
+
+    console.log("EROR: " + err)
+})
+app.post('/api/signin', (request, response) => {
+    const {lat , lng} = request.body
+
+    const data = {
+        latitude : lat,
+        longitude : lng
+    }
+
+
+})
+app.listen(port, function() {
+    console.log("Listening...")
+})
