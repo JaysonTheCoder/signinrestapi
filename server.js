a7980106b6e168e8aaf0b6e11c744b125c596a84
JaysonTheCoder <jaysonbonina6@gmail.com>
2024-04-05 19:11:48 -0700

    hay nako may pag asa paba?

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 const express = require("express")
 const app = express() 
 const mysql = require("mysql")
-const cors = require('cors')
+const cors = require("cors")
 const port = 4040
-app.use(cors())
 app.use(express.json())
-app.use(express.urlencoded({extended: false}))
-
+app.use(cors())
 
 const connection = mysql.createConnection({
     host    : 'sql6.freesqldatabase.com',
