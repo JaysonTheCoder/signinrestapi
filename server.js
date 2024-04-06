a188ed12a89ba2265648e058394cd7630e27cfcf
JaysonTheCoder <jaysonbonina6@gmail.com>
2024-04-05 22:30:36 -0700

    ajsfasf

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express() 
 const mysql = require("mysql")
 const cors = require("cors")
-const port = 10000
+const port = process.env.PORT||10500
 app.use(express.json())
 app.use(cors())
 
